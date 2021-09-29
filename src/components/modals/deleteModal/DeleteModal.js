779840f88429b831fc43faf53ef11482a871a3a9
Avdhoot <avdhoot.980@gmail.com>
2021-09-29 22:11:19 +0530

    Added delete modal.

---
+++ src/components/modals/deleteModal/DeleteModal.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Modal, Button, Form, Row, Col } from "react-bootstrap";
+
+const DeleteModal = ({ show, onHide, handleDeleteClick }) => {
+  return (
+    <Modal
+      show={show}
+      onHide={onHide}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Body className="delete-modal-container">
+        <div>
+          <h4>Do you want to delete this task?</h4>
+        </div>
+        <div>
+          <Button
+            className="delete-btn mr-4 mt-3"
+            onClick={handleDeleteClick(null)}
+          >
+            Yes
+          </Button>
+          <Button className="done-btn mt-3" onClick={onHide}>
+            No
+          </Button>
+        </div>
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default DeleteModal;
